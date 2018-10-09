5f5d320639647b72044f2906855fb0412e06e363
Daniel J. Umpierrez <umpierrez@pm.me>
2018-10-09 21:58:17 +0200

    Update __init__.py

--- dreamdict/__init__.py
+++ dreamdict/__init__.py
@@ -7,7 +7,7 @@ from dreamdict.core import DreamDict
 __project__ = 'DreamDict'
 __package__ = 'dreamdict'
 __version__ = '0.1.0'
-__license__ = 'MIT'
+__license__ = 'UNLICENSE'
 __description__ = __doc__
 __email__ = 'umpierrez@pm.me'
 __site__ = 'https://github.com/havocesp/{}'.format(__package__)
